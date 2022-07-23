58f6db2bc34403cb87e4f565fb43c2ff786b8d89
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-07-22 20:29:06 -0400

    Created a footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<nav className='footer'>
+				<a href='https://www.facebook.com/george.gmoney' target='_blank'>
+					Facebook
+				</a>
+				<a href='https://github.com/gkojunior' target='_blank'>
+					Github
+				</a>
+				<a
+					href='https://www.linkedin.com/in/george-owusu-2a853411b/'
+					target='_blank'>
+					Linkedin
+				</a>
+			</nav>
+		</footer>
+	);
+};
+
+export default Footer;
